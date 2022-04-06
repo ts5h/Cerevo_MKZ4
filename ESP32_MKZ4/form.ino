dd7a68016b879e27630f3b634db17ede1cfd2d05
at4 <at4@jc4.so-net.ne.jp>
2022-04-06 22:53:41 +0200

    Change html specification to raw literal format

--- ESP32_MKZ4/form.ino
+++ ESP32_MKZ4/form.ino
@@ -1,95 +1,138 @@
 // HTML string for controlling MKZ4 from smartphone
-String form =
-    "<html>"
-    "<head>"
-    "<meta name='viewport' content='width=device-width, initial-scale=1, "
-    "minimum-scale=1, maximum-scale=1'>"
-    "<title>MKZ4 Controller</title>"
-    "<style type='text/css'>"
-    "* { margin: 0; padding: 0; }"
-    "body { background-color: #0097c1; }"
-    ".value {"
-    "position: fixed;"
-    "width: 100%;"
-    "top: 50px;"
-    "text-align: center;"
-    "color: white;"
-    "font-family: sans-serif;"
-    "font-size: 200%;"
-    "font-weight: normal;"
-    "text-transform: capitalize;"
-    "}"
-    "</style>"
-    "</head>"
-    "<body>"
-    "<div id='value' class='value'>connected</div>"
-    "<form id='form' action='' target='tif'>"
-    "<iframe src='javascript: false;' style='display: none' name='tif' "
-    "id='tif'></iframe>"
-    "</form>"
-    "<script type='text/javascript'>"
-    "var offset = 50;"
-    "document.body.style.height = document.body.clientHeight + offset + 'px';"
-    "document.body.style.width = document.body.clientWidth + offset + 'px';"
-    "document.getElementsByTagName('html')[0].style.height = "
-    "document.body.style.height + 'px';"
-    "document.getElementsByTagName('html')[0].style.width = "
-    "document.body.style.width + 'px';"
-    "var moveHomePosition = function () {"
-    "document.body.scrollTop = offset / 2;"
-    "document.body.scrollLeft = offset / 2;"
-    "};"
-    "setTimeout(moveHomePosition, 300);"
-    "var startX = 0;"
-    "var startY = 0;"
-    "var command = '/stop';"
-    "var threshold = 40;"
-    "var esp_port = 'http://192.168.4.1:8080';"
-    "var el_form = document.getElementById('form');"
-    "document.body.ontouchstart = function (event) {"
-    "startX = event.touches[0].clientX;"
-    "startY = event.touches[0].clientY;"
-    "};"
-    "document.body.ontouchmove = function (event) {"
-    "var x = parseInt(event.touches[0].clientX - startX);"
-    "var y = parseInt(event.touches[0].clientY - startY);"
-    "var url = null;"
-    "if (x < threshold * -1) {"
-    "if (y < threshold * -1) {"
-    "url = '/left FORWARD';"
-    "} else if (y > threshold) {"
-    "url = '/left back';"
-    "} else {"
-    "url = '/left';"
-    "}"
-    "} else if (x > threshold) {"
-    "if (y < threshold * -1) {"
-    "url = '/right FORWARD';"
-    "} else if (y > threshold) {"
-    "url = '/right back';"
-    "} else {"
-    "url = '/right';"
-    "}"
-    "} else {"
-    "if (y < threshold * -1) {"
-    "url = '/FORWARD';"
-    "} else if (y > threshold) {"
-    "url = '/back';"
-    "}"
-    "}"
-    "if (command != url && url) {"
-    "el_form.action = esp_port + url.replace(/<br>| /, '');"
-    "el_form.submit();"
-    "document.getElementById('value').innerHTML = url.replace(/\\//, '');"
-    "}"
-    "command = url;"
-    "};"
-    "document.body.ontouchend = function (event) {"
-    "el_form.action = esp_port + '/stop';"
-    "el_form.submit();"
-    "setTimeout(moveHomePosition, 50);"
-    "document.getElementById('value').innerHTML = 'stop';"
-    "};"
-    "</script>"
-    "</body>"
-    "</html>";
+String form = R"#(
+<html>
+<head>
+  <meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1'>
+  <title>MKZ4 Controller</title>
+  <style type='text/css'>
+    * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+    }
+
+    html, body {
+      width: 100%;
+      height: 100%
+    }
+
+    body {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      height: 100%;
+      background-color: #0097c1;
+    }
+
+    .value {
+      position: fixed;
+      width: 100%;
+      top: 30px;
+      text-align: center;
+      color: white;
+      font-family: sans-serif;
+      font-size: 1rem;
+      font-weight: normal;
+      text-transform: capitalize;
+    }
+
+    .controller__wrapper {
+      position: relative;
+      width: 252px;
+      height: 252px;
+      border: 1px solid rgba(255, 255, 255, 0.5);
+    }
+
+    .controller {
+      position: absolute;
+      width: 50px;
+      height: 50px;
+      left: calc(50% - 25px);
+      top: calc(50% - 25px);
+      background: rgba(255, 255, 255, 0.2);
+      border: 1px solid white;
+      border-radius: 50%;
+    }
+  </style>
+</head>
+
+<body>
+  <div id='value' class='value'>connected</div>
+  <form id='form' action='' target='tif'>
+    <iframe src='javascript: false;' style='display: none' name='tif' id='tif'></iframe>
+  </form>
+  <div id='controller__wrapper' class='controller__wrapper'>
+    <div id='controller' class='controller'></div>
+  </div>
+
+  <script type='text/javascript'>
+    var espPort = 'http://192.168.4.1:8080';
+    var command = '/stop';
+    
+    var value = document.getElementById('value');
+    var form = document.getElementById('form');
+    var wrapper = document.getElementById('controller__wrapper');
+    var controller = document.getElementById('controller');
+
+    var touchFlag = false;
+    var controllerOffset = controller.clientWidth / 2;
+    var wrapperOffsetX = wrapper.offsetLeft;
+    var wrapperOffsetY = wrapper.offsetTop;
+    var startX = 100;
+    var startY = 100;
+    var maxX = 200;
+    var maxY = 200;
+
+    var showValue = function (state) {
+      value.innerHTML = state;
+    };
+
+    var moveHomePosition = function () {
+      controller.style.left = startX;
+      controller.style.top = startY;
+    };
+
+    wrapper.ontouchstart = function (e) {
+      e.preventDefault();
+      touchFlag = true;
+    };
+
+    wrapper.ontouchmove = function (e) {
+      if (!touchFlag) return;
+      e.preventDefault();
+
+      var touch = e.touches[0];
+      var controllerLeft = touch.clientX - wrapperOffsetX - controllerOffset;
+      var controllerTop = touch.clientY - wrapperOffsetY - controllerOffset;
+      var x = 0;
+      var y = 0;
+
+      if (controllerLeft <= 0) controllerLeft = 0;
+      if (controllerLeft >= maxX) controllerLeft = maxX;
+      if (controllerTop <= 0) controllerTop = 0;
+      if (controllerTop >= maxY) controllerTop = maxY;
+
+      controller.style.left = controllerLeft;
+      controller.style.top = controllerTop;
+      x = controllerLeft - startX;
+      y = (controllerTop - startY) * -1;
+
+      form.action = espPort + '/move?x=' + x + '&y=' + y;
+      form.submit();
+    };
+
+    wrapper.ontouchend = function (e) {
+      e.preventDefault();
+      touchFlag = false;
+      form.action = espPort + '/stop';
+      form.submit();
+      setTimeout(moveHomePosition, 50);
+    }
+
+    window.onload = function () {
+      moveHomePosition();
+    };
+  </script>
+</body>
+</html>)#";
